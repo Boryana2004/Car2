537200bb394335935ac9e97f21f1fbe533c152f8
User <User@DESKTOP-64J817K>
2021-11-22 23:11:01 +0200

    Add project files.

---
+++ Car2/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car2
+{
+    class Car
+    {
+        private int yearOfProduction;
+        private int engineVolume;
+
+        public int YearOfProduction
+        { get { return this.yearOfProduction; }
+          set { this.yearOfProduction = value; } 
+        }
+
+        public int EngineVolume 
+        { get { return this.engineVolume; } 
+          set {this.engineVolume = value; } 
+        }
+    }
+}
---
+++ Car2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Car2;
+
+namespace Car2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car car1 = new Car();
+            Console.Write("Year of production: ");
+            car1.YearOfProduction = int.Parse(Console.ReadLine());
+            Console.Write("Engine volume: ");
+            car1.YearOfProduction = int.Parse(Console.ReadLine());
+            if ()
+            {
+
+            }
+            decimal annualTax = 
+
+        }
+    }
+}
